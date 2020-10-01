506a13e4e9102222dd0288d9f944b60cae6292e2
mikeyruc123 <mwruc2000@aol.com>
2020-10-01 14:52:09 -0400

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-char *token(); // iterates through input string and creates the tokens
+char **token(); // iterates through input string and creates the tokens
 void type(); // given each token, determine the type of said token and print to screen
 
 
