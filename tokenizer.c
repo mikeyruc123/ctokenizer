6dde2fb0a45ce4ee6fa3d84cf6fcfe0b02cf86fe
mikeyruc123 <mwruc2000@aol.com>
2020-10-01 15:02:18 -0400

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -3,16 +3,9 @@
 #include <stdio.h>
 #include <ctype.h>
 
-
-
 int main(char *argv[]){
 
-  char *string = malloc(sizeof(argv[1]));
-  
-  int i;
-  for (i = 0; i < sizeof(argv[1]); i++){
-    string[i] = argv[1][i];
-  }
+  int length = strlen(argv[1]);
   
   return 0;
 
