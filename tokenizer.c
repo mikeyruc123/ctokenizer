616d2e443022dabbbe582011845f672a175bdcd5
mikeyruc123 <mwruc2000@aol.com>
2020-10-01 15:00:23 -0400

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -7,6 +7,13 @@
 
 int main(char *argv[]){
 
+  char *string = malloc(sizeof(argv[1]));
+  
+  int i;
+  for (i = 0; i < sizeof(argv[1]); i++){
+    string[i] = argv[1][i];
+  }
+  
   return 0;
 
 }
