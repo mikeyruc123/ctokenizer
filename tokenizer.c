852973cdeabf2b92b15f42377f214dfc70a14b27
Michael Rucando <michaelrucando@Michaels-Air.fios-router.home>
2020-10-01 14:45:51 -0400

    Updated tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <ctype.h>
 
+char *token(); // iterates through input string and creates the tokens
+void type(); // given each token, determine the type of said token and print to screen
+
+
+
+
 int main(){
 
   return 0;
